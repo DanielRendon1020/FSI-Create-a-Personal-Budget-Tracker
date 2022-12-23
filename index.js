f1730f0fff185e5eb0e7f4c6d1db17d9c5c5fe3e
DanielRendon1020 <d.danny1020@gmail.com>
2022-12-22 20:30:12 -0800

    reversed weekly

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+for (let i = 2; i >= 0; i--){
+    let stringAnswer = window.prompt(weeklyExpenseQuestions[i])
+    console.log(stringAnswer)
+    let numberAnswer = parseFloat(stringAnswer)
+    weeklyExpenses += numberAnswer
+}
+for (let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let stringAnswer = window.prompt(monthlyExpenseQuestions[i])
+    console.log(stringAnswer)
+    let numberAnswer = parseFloat(stringAnswer)
+    monthlyExpenses += numberAnswer
+}
+for (let i = 0; i < annualExpenseQuestions.length; i++){
+    let stringAnswer = window.prompt(annualExpenseQuestions[i])
+    console.log(stringAnswer)
+    let numberAnswer = parseFloat(stringAnswer)
+    annualExpenses += numberAnswer
+}
